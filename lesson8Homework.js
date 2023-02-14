afa24f639c8896ecf186c37f1f378130e5621ce5
mecastor <mecastor87@gmail.com>
2023-02-14 22:16:11 +0000

    Uploaded required function & instructions

---
+++ lesson8Homework.js
@@ -0,0 +1,8 @@
+// Create a function that takes in three numbers and returns their sum. Call the function with different numbers and log the result to the console. Save this exercise within a GitHub repository and share the link to the repository in this thread.
+
+const sumThreeNumbers = (num1, num2, num3) => {
+    return num1 + num2 + num3;
+}
+
+console.log(sumThreeNumbers(2, 4, 70));
+console.log(sumThreeNumbers(4, 45, 67));
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Readme 
+Create a function that takes in three numbers and returns their sum. Call the function with different numbers and log the result to the console. Save this exercise within a GitHub repository and share the link to the repository in this thread.
